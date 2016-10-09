baaea5ed7c332ac9b7531f551acc975717c6ccdf
= <hmgaar@gmail.com>
2016-10-09 02:27:00 -0400

    now posting to rails api

--- public/scripts/todo_app.js
+++ public/scripts/todo_app.js
@@ -1,9 +1,12 @@
+// var todos_endpoint = "https://desolate-spire-35851.herokuapp.com/todos"
+var todos_endpoint = "https://todo-ang.herokuapp.com/todos"
 var TodoApp = React.createClass({
   render: ()=> {
+    // <AddTodo endpoint="https://desolate-spire-35851.herokuapp.com/todos"/>
     return (
       <div>
-        <TodosList endpoint="https://todo-ang.herokuapp.com/todos"/>
-        <AddTodo endpoint="https://todo-ang.herokuapp.com/todos"/>
+        <TodosList endpoint={todos_endpoint}/>
+        <AddTodo endpoint={todos_endpoint}/>
       </div>
     );
   }
@@ -14,7 +17,7 @@ var TodosList = React.createClass({
     return(
       <div className="some_class">
        <h1>Todo React App</h1>
-       <Todo className="todos_list" data={this.state.todos}/>
+       <Todo className="todos_list" endpoint={todos_endpoint} data={this.state.todos}/>
       </div>
     );
   },
@@ -47,8 +50,12 @@ var Todo = React.createClass({
       return(
         <div className="todo" key={todo.id}>
           <form id={todo.id} onSubmit={this.editTodo}>
-            <input id={todo.id}  type="text" onChange={this.setTodo} className="todo" placeholder="What needs to be done?" defaultValue={todo.description} />
+            <input id={todo.id}  type="text" onChange={this.setTodo} className="todo" defaultValue={todo.description} />
           </form>
+          <form id={todo.id} onSubmit={this.delete}>
+            <input type="submit" className="delete_todo" defaultValue="delete" />
+          </form>
+
         </div>
         );
       });
@@ -61,7 +68,13 @@ var Todo = React.createClass({
     },
     setTodo:function(event){
       event.preventDefault();
+      console.log(this.state);
       this.setState({id: event.target.id, description: event.target.value});
+      console.log(this.state);
+    },
+    setID:function(id){
+      console.log("");
+      this.setState({id:id});
     },
     componentDidMount:function(){
 
@@ -69,17 +82,64 @@ var Todo = React.createClass({
     editTodo:function(event){
       event.preventDefault();
       if(this.state){
-        event.target.id == this.state.id ? this.put(event.target.id) : console.log("this state: null or you havent changed the state");
+        console.log("if");
+        console.log(event.target.id);
+        console.log(this.state.id);
+        event.target.id == this.state.id ? this.put() : console.log("this state: null or you havent changed the state");
       }
       else {
+        console.log("else");
         return;
       }
+      console.log("this.state");
+      console.log(this.state);
     },
-    put:function(todo){
-      console.log("todo in put");
-      console.log(todo);
-    }
+    put:function(){
+      var todo = {todo: {description: this.state.description}};
+      var request = new Request(this.props.endpoint +"/"+ this.state.id,{
+      method: "PUT",
+      mode: "cors",
+      redirect: 'follow',
+      headers: new Headers({'Content-Type': 'application/json'}),
+      body: JSON.stringify(todo)
+      });
+      fetch(request).then(function(res){ console.dir( "res", res ); });
+  },
+  delete:function(event){
+    event.preventDefault();
+    this.setID(event.target.id).toPromise().then(console.log(this.state));
+    // console.log(this.state);
+    // if(this.state.id){
+    //   var request = new Request(this.props.endpoint +"/"+ this.state.id,{
+    //   method: "DELETE",
+    //   mode: "cors",
+    //   redirect: 'follow',
+    //   headers: new Headers({'Content-Type': 'application/json'}),
+    //   body: JSON.stringify(todo)
+    //   });
+    //   fetch(request).then(function(res){ console.dir( "res delete request", res ); });
+    // }
+    // else {
+    //   console.log(this.state);
+    // }
+
+
+    // var request = new Request(this.props.endpoint +"/"+ this.state.id,{
+    // method: "DELETE",
+    // mode: "cors",
+    // redirect: 'follow',
+    // headers: new Headers({'Content-Type': 'application/json'}),
+    // body: JSON.stringify(todo)
+    // });
+    // fetch(request).then(function(res){ console.dir( "res delete request", res ); });
+  }
+
+
 });
+// return this.http
+//     .put(this.todo_end + '/' + todo.id, JSON.stringify(updated_todo), { headers: headers })
+//     .toPromise()
+//     .then(function (res) { return res.json(); });
 
 var AddTodo = React.createClass({
   getInitialState:function(){
@@ -104,15 +164,9 @@ var AddTodo = React.createClass({
         description:this.state.description
       }
     }
-    return fetch(this.props.endpoint,{
-        method:"POST",
-        headers:{
-          "Content-Type": "application/json"
-        },
-        body:JSON.stringify(todo)
-    }).then((res)=>{return res.json()}).then((res)=> {console.log("res :", res);},(error)=>{console.log("Error occurred: ", error);})
+    var request = new Request(this.props.endpoint, {method:"POST", mode:"cors", headers: new Headers({"Content-Type":"application/json"}), body:JSON.stringify(todo)});
+    return fetch(request).then((res)=>{return res.json()}).then((res)=> {console.log("res :", res) }, (error)=>{console.log("Error occurred: ", error)})
   }
-
 });
 
 ReactDOM.render(
@@ -179,3 +233,19 @@ ReactDOM.render(
 //       </li>
 //     </div>
 //   </div>
+
+// FETCH WAY
+// put:function(){
+//   console.log("todo in put");
+//   var todo = {
+//     todo: {
+//       id:this.state.id,
+//       description: this.state.description
+//     }
+//   }
+//   console.log(todo);
+//   var request = new Request(this.props.endpoint+"/"+this.state.id, {method:"PUT", mode:"cors", headers: new Headers({"Content-Type":"application/json"}), body:JSON.stringify(todo), redirect:"follow"});
+//   console.log("request");
+//   console.log(request);
+//   fetch(request).then((res)=>{return res.json()}).then((res)=> {console.log("res :", res) }, (error)=>{console.log("Error occurred: ", error)});
+// }
