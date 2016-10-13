eab9b26f6a5262485df34ff1232f51bb40926feb
= <hmgaar@gmail.com>
2016-10-13 04:38:03 -0400

    Quotox React Web created

---
+++ src/quotox.js
@@ -0,0 +1,23 @@
+// Quotox App Structure
+
+class QuotoxApp extends React.Component {
+// ALL Class/Components here
+}
+
+class Quote extends React.Component {
+// Here we
+// Quote of the day
+// A Lion does not concern himself with the opinion of Sheep
+}
+
+class Detox extends React.Component {
+// Here we show how many clean days
+}
+
+class AddDetox extends React.Component {
+// Here we add our Detox object
+}
+
+class AddQuote extends React.Component {
+// Here we can add quotes only for us
+}
